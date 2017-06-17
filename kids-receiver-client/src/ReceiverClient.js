7548fdfd5c057afbf87db9dcea518e31c37bc1f3
Josh Martin <joshmartin5001@gmail.com>
2017-06-17 04:27:01 -0400

    -m
    updates views

--- kids-receiver-client/src/ReceiverClient.js
+++ kids-receiver-client/src/ReceiverClient.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+
+import AppNav from './AppNav';
 import logo from './logo.svg';
 import './App.css';
+import Section from './components/Section'
+
 import {BrowserRouter,  Route, Link} from 'react-router-dom';
-import AppNav from './AppNav';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 class ReceiverClient extends Component {
   render() {
@@ -10,9 +14,19 @@ class ReceiverClient extends Component {
       <MuiThemeProvider>
         <BrowserRouter>
           <AppNav>
+            <div id="section">
+            <Section style={{flex:1,marginRight:20,marginLeft:20}}>
+              <div>
+                <h1>hello world</h1>
+              </div>
+            </Section>
+            <Section style={{flex:1}}>
+              <div>
+                <h1>hello world</h1>
+              </div>
+            </Section>
+          </div>
             <Route exact path="/" />
-            {/*<Route path="/docs" component={DocumentViewer}/>*/}
-            {/*<Route path="/mou" component={MOUAgreement}/>*/}
           </AppNav>
         </BrowserRouter>
       </MuiThemeProvider>
