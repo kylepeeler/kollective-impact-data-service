12a2c2c0eb3b86d5aa2372f7353d9cc8b79fe251
jake-white <squirrelboy1225@gmail.com>
2017-06-17 13:39:44 -0400

    added custom icons, fixed layout

--- kids-sender-client/src/DocViewer.js
+++ kids-sender-client/src/DocViewer.js
@@ -9,6 +9,10 @@ import Avatar from 'material-ui/Avatar';
 import Subheader from 'material-ui/Subheader';
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import EditorInsertDriveFile from 'material-ui/svg-icons/editor/insert-drive-file';
+import MapsLocalHospital from 'material-ui/svg-icons/maps/local-hospital';
+import SocialPerson from 'material-ui/svg-icons/social/person';
+import SocialSchool from 'material-ui/svg-icons/social/school';
+import Paper from 'material-ui/Paper';
 import {blue500, yellow600} from 'material-ui/styles/colors';
 import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
 import './DocViewer.css'
@@ -23,16 +27,34 @@ class DocViewer extends React.Component {
         {
           "title": "Birth Certificate",
           "src": "http://dhss.delaware.gov/dph/ss/files/birth.pdf",
-          "uploader": "John Smith",
+          "uploader": "Kyle Peeler",
           "timestamp": "1497715391",
-          "description": "birth certificate sample"
+          "description": "Birth certificate sample",
+          "category": "personal"
         },
         {
           "title": "Social Security Card",
           "src": "https://www.ssa.gov/pubs/EN-05-10002.pdf",
-          "uploader": "John Smith",
+          "uploader": "Jake White",
           "timestamp": "1497715391",
-          "description": "ssn sample"
+          "description": "SSN sample",
+          "category": "personal"
+        },
+        {
+          "title": "High School Diploma",
+          "src": "https://www.in.gov/icc/files/Core_40_and_Honors_Diploma_Requirements.pdf",
+          "uploader": "Trent Spice",
+          "timestamp": "1497715391",
+          "description": "Education sample",
+          "category": "education"
+        },
+        {
+          "title": "Medical Records",
+          "src": "http://www.miec.com/portals/0/pubs/medicalrec.pdf",
+          "uploader": "Andy Harris",
+          "timestamp": "1497715391",
+          "description": "Medical sample",
+          "category": "medical"
         }
       ]
     }
@@ -45,20 +67,42 @@ showDocument(e, index){
   });
 }
 
+epochToDate(seconds) {
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(seconds);
+  return d;
+}
+
 render(){
   const documentViewerStyle = {
     height: 400
+  }
+  const icons = {
+    "personal": (<SocialPerson />),
+    "education": (<SocialSchool />),
+    "medical": (<MapsLocalHospital />)
   }
   const props = {
     allowFullScreen: true,
     src: this.state.files[this.state.currentFileOpen].src,
     className: 'document-viewer'
   };
+
+    const listStyle = {
+      width: '30%',
+      padding: 20,
+      float: 'left'
+    };
+    const docStyle = {
+      width: '70%',
+      padding: 20,
+      float: 'right'
+    };
 const FileListItems = this.state.files.map((file, index) =>
   <ListItem
   src={this.state.files[index].src}
   key={index}
-  leftAvatar={<Avatar icon={<EditorInsertDriveFile />}/>}
+  leftAvatar={<Avatar icon={icons[this.state.files[index].category]}/>}
   rightIcon={<ActionInfo />}
   primaryText={this.state.files[index].title}
   secondaryText={this.state.files[index].description + ', Uploaded By: ' + this.state.files[index].uploader}
@@ -67,15 +111,26 @@ const FileListItems = this.state.files.map((file, index) =>
 
   return (
     <div>
-      <List className="document-list">
-      </List>
-      <Divider inset={true} />
-      <List>
-        <Subheader inset={true}>Files</Subheader>
-        {FileListItems}
-      </List>
+      <Paper zDepth={2} style={listStyle}>
+        <List className="document-list">
+        </List>
+        <Divider inset={true} />
+        <List>
+          <Subheader inset={true}>Files</Subheader>
+          {FileListItems}
+        </List>
+      </Paper>
+      <Paper zDepth={2} style={docStyle}>
+        <div style={listStyle}>
+          <h2>{this.state.files[this.state.currentFileOpen].title}</h2>
+          <p>{"Uploaded on: " + this.epochToDate(this.state.files[this.state.currentFileOpen].timestamp)}</p>
+          <p>{"By: " + this.state.files[this.state.currentFileOpen].uploader}</p>
+          <p>{"Description: " + this.state.files[this.state.currentFileOpen].description}</p>
 
-      <Component {...props} />
+        </div>
+        <Component  {...props} />
+      </Paper>
+
     </div>
   );
 }
